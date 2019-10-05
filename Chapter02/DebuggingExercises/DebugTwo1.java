2e1535f9c7444f023cfe9a83a96a085f27542f96
Firefly842 <stevenwei05@gmail.com>
2019-10-05 14:48:43 -0700

    Update DebugTwo1.java

--- Chapter02/DebuggingExercises/DebugTwo1.java
+++ Chapter02/DebuggingExercises/DebugTwo1.java
@@ -2,9 +2,9 @@ public class DebugTwo1
 {
    public static void main(String[] args)
    {
-      int oneInt = 315.0;
+      int oneInt = 315;
       double oneDouble = 12.4;
-      char oneChar = A;
+      char oneChar = 'A';
       System.out.println("The int is ");
       System.out.print(oneInt);
       System.out.println("The double is ");
