94e2b4f49975a45f8f5c8eae25e9451e66303043
Firefly842 <stevenwei05@gmail.com>
2019-10-19 14:58:50 -0700

    Update DebugThree2.java

--- Chapter03/DebuggingExercises/DebugThree2.java
+++ Chapter03/DebuggingExercises/DebugThree2.java
@@ -6,16 +6,16 @@ public class DebugThree2
       int a = 2, b = 5, c = 10;
       add(a, b);
       add(b, c);
-      subtract(c, a);            
+      subtract(c, a);
    }
-   public static void add()
+   public static void add(int a, int b)
    {
       System.out.println("The sum of " + a +
-         and + b + is + a + b);
+         " and " + b + " is " + (a + b));
    }
-   public void subtract()
+   public static void subtract(int c, int a)
    {
       System.out.println("The difference between " +
-        a + and + b + is + a - b);
+        c + " and " + a + " is " + (c - a));
    }
 }
