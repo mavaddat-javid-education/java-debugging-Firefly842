6214ede193ccc7ccac9865fdcbe837c1df6b5a13
Firefly842 <stevenwei05@gmail.com>
2019-12-14 14:36:04 -0800

    fixed Debug 5 4

--- Chapter05/DebuggingExercises/DebugFive4.java
+++ Chapter05/DebuggingExercises/DebugFive4.java
@@ -7,24 +7,24 @@ public class DebugFive4
    {
       int one, two, three, four;
       String str, output;
-      str = JOptionPane.showInputDialog(null,"Enter an integer");
-      one = Integer.parseInt();
-      str = JOptionPane.showInputDialog(null,"Enter an integer");
-      two = Integer.parseInt();
-      str = JOptionPane.showInputDialog(null,"Enter an integer");
-      two = Integer.parseInt();
-      str = JOptionPane.showInputDialog(null,"Enter an integer");
-      four = Integer.parseInt();
-      if(one > two & one >= three && one < four)
+      str = JOptionPane.showInputDialog(null,"Enter first integer");
+      one = Integer.parseInt(str);
+      str = JOptionPane.showInputDialog(null,"Enter second integer");
+      two = Integer.parseInt(str);
+      str = JOptionPane.showInputDialog(null,"Enter third integer");
+      three = Integer.parseInt(str);
+      str = JOptionPane.showInputDialog(null,"Enter fourth integer");
+      four = Integer.parseInt(str);
+      if(one > two && one > three && one > four)
          output = "Highest is " + one;
       else
-         if(two > one || two > three && two > four)
+         if(two > one && two > three && two > four)
             output = "Highest is " + two;
          else
-           if(three > one && three > two || three == four)
+           if(three > one && three > two && three > four)
               output = "Highest is " + three;
            else
-              output = "Highest is " + three;
+              output = "Highest is " + four;
       JOptionPane.showMessageDialog(null, output);
    }
 }
