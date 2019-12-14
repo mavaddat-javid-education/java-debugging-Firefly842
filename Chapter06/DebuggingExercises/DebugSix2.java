ff067cd8be98d9102acdd5a0d4b61edefd7ba7eb
Firefly842 <stevenwei05@gmail.com>
2019-12-14 14:59:58 -0800

    fixed Debug 6 2

--- Chapter06/DebuggingExercises/DebugSix2.java
+++ Chapter06/DebuggingExercises/DebugSix2.java
@@ -5,17 +5,19 @@ public class DebugSix2
 {
    public static void main(String args[])
    {
-      char letter;
+      char letter = ' ';
       int a;
       final int MIN = 65;
       final int MAX = 122;
       final int STOPLINE1 = 85;
       final int STOPLINE2 = 122;
       for(a = MIN; a <= MAX; a++)
+       { 
         letter = (char)a;
         System.out.print("  " + letter);
-        if((a == STOPLINE1) & (a == STOPLINE2))
+        if((a == STOPLINE1) || (a == STOPLINE2))
           System.out.println();
-      System.out.println("\nEnd of application")
+        }
+      System.out.println("\nEnd of application");
     }
 }
